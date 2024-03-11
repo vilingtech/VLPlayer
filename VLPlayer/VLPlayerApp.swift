5426fef9a640ab0cca9828fd349ad56c9740f5ed
luoshao <luoshao118@gmail.com>
2024-03-11 20:42:34 +0100

    Initial Commit

---
+++ VLPlayer/VLPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VLPlayerApp.swift
+//  VLPlayer
+//
+//  Created by Zhiling LUO on 11/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct VLPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
